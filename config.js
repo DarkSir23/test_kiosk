908dfe2b68640888fe5984ab41efb6484641a829
DarkSir23 <jason@fortsackville.com>
2020-07-27 10:47:14 -0400

    Second commit

---
+++ config.js
@@ -0,0 +1,11 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "Whitko Kiosk",
+   "homepage": "https://whitko.org",
+   "enableNavBttns": false,
+   "enableHomeBttn": false,
+   "enableReloadBttn": false,
+   "enableLogoutBttn": false,
+   "kioskEnabled": true
+};
